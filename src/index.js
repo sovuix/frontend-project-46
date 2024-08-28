8660f431b518ebc0eae0b7a387cb9d7ecfdb9a09
sovuix <sovuix@gmail.com>
2024-08-28 16:28:39 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import parse from './parser.js';
 const getAbsolutePath = (filepath) => path.resolve(process.cwd(), filepath);
 const getFileFormat = (filepath) => path.extname(filepath).slice(1);
 
-//
 const getFileData = (filepath) => {
   const absoluteFilePath = getAbsolutePath(filepath);
   const format = getFileFormat(filepath);
   const data = parse(fs.readFileSync(absoluteFilePath, 'utf8'), format);
+
   return data;
 };
 
