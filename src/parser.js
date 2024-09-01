ee0417743869e0a46092f1dbce9850e903332be9
Slava <sovuix@gmail.com>
2024-09-01 16:00:06 +0300

    add .yaml/.yml

--- src/parser.js
+++ src/parser.js
@@ -1,7 +1,24 @@
-const parsers = {
-  json: JSON.parse,
-};
+// const parsers = {
+//   json: JSON.parse,
+// };
 
-const parse = (data, format) => parsers[format](data);
+// const parse = (data, format) => parsers[format](data);
+import yaml from 'js-yaml';
+
+const parse = (data, format) => {
+  switch (format) {
+    case 'json':
+      return JSON.parse(data);
+    case 'yaml':
+      return yaml.load(data);
+    case 'yml':
+      return yaml.load(data);
+    default:
+      throw new Error('Unknown file extension!');
+  }
+}
+
+
 
 export default parse;
+// export default parse;
