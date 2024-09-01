367703309a640a5554ea05108192246462ca06d6
Slava <sovuix@gmail.com>
2024-09-01 15:58:22 +0300

    add .yaml format

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -4,13 +4,20 @@ import fs from 'node:fs';
 
 const fileJson1 = './__fixtures__/file1.json';
 const fileJson2 = './__fixtures__/file2.json';
+const fileYaml1 = './__fixtures__/file1.yaml';
+const fileYaml2 = './__fixtures__/file2.yaml';
 
-const data1 = getFileData(fileJson1);
-const data2 = getFileData(fileJson2);
+
+const data1Json = getFileData(fileJson1);
+const data2Json = getFileData(fileJson2);
+const data1Yaml = getFileData(fileYaml1);
+const data2Yaml = getFileData(fileYaml2);
 
 const expectedFile = fs
   .readFileSync('./__fixtures__/expected_JSON.txt', 'utf-8');
 
 test('comparing files with nesting', () => {
-  expect(compareKeys(data1, data2)).toEqual(expectedFile);
+  expect(compareKeys(data1Json,data2Json)).toEqual(expectedFile);
+  expect(compareKeys(data1Yaml,data2Yaml)).toEqual(expectedFile);
+
 });
