c8f59c808011ec64fd01981f1f9167f2ec599878
Slava <sovuix@gmail.com>
2024-11-09 17:32:34 +0300

    fix and change

--- src/compareAst.js
+++ src/compareAst.js
@@ -1,49 +1,46 @@
 
-// const mapping = {
-//   added: (node, depth) => {},
-//   deleted: (node, depth) => {},
-// }
-// const res = mapping[node, type](node, depth)
+const compareAst = (node) => {
+  const formatValue = (value) => {
+    if (typeof value === 'object' && value !== null) {
+      // TODO: неправильно, JSON.stringify не учитывает отступы
+      return JSON.stringify(value, null, 2);
+    }
+    if (typeof value === 'string') {
+      return `"${value}"`;
+    }
 
-// 
+    return String(value);
+  };
 
-const compareAst = (node) => { 
-  const res = []; 
- 
-  const formatValue = (value) => { 
-    if (typeof value === 'object') { 
-      return JSON.stringify(value, null, 2); 
-    } 
-    return value; 
-  }; 
- 
-  const iter = (node, depth = 0) => { 
-    const indent = '  '.repeat(depth); 
- 
-    if (node.type === 'added') { 
-      res.push(`${indent}+ ${node.key}: ${formatValue(node.value)}`); 
-    } else if (node.type === 'changed') { 
-      res.push(`${indent}- ${node.key}: ${formatValue(node.oldValue)}`); 
-      res.push(`${indent}+ ${node.key}: ${formatValue(node.newValue)}`); 
-    } else if (node.type === 'nested') { 
-      res.push(`${indent}${node.key}: {`); 
-      node.children.map((child) => iter(child, depth + 2)); 
-      res.push(`${indent}}`); 
-    } else if (node.type === 'deleted') { 
-      res.push(`${indent}- ${node.key}`); 
-    } else if (node.type === 'notModified') { 
-      res.push(`${indent} ${node.key}: ${formatValue(node.value)}`); 
-    } 
-  }; 
- 
-  if (node.type === 'root') { 
-    res.push(node.key); 
-    node.children.forEach((child) => iter(child, 0)); 
-  } else { 
-    iter(node, 0); 
-  } 
-   
-  return res.join('\n'); 
+  const iter = (node, depth = 0) => {
+    const indent = '  '.repeat(depth);
+    const { type } = node;
+
+    if (type === 'added') {
+      return [`${indent}+ ${node.key}: ${formatValue(node.value)}`];
+    } else if (type === 'changed') {
+      return [
+          `${indent}- ${node.key}: ${formatValue(node.oldValue)}`,
+          `${indent}+ ${node.key}: ${formatValue(node.newValue)}`,
+      ];
+    } else if (type === 'nested') {
+      return [`${indent}${node.key}: {\n${node.children.flatMap((child) => iter(child, depth + 1)).join(`\n`)}\n${indent}}`];
+    } else if (type === 'deleted') {
+      return [`${indent}- ${node.key}: ${formatValue(node.value)}`];
+    } else if (type === 'notModified') {
+      return [`${indent}  ${node.key}: ${formatValue(node.value)}`];
+    }
+
+    return '';
+  }
+
+  if (node.type === 'root') {
+    return `{\n${node.children.flatMap((child) => iter(child, 1)).join('\n')}\n}`;
+  }
+
+  return '';
 };
+
+// console.log(compareAst(ast));
 
 export default compareAst;
