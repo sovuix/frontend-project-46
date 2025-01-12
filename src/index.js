eb7c6f0bad554cb8f3cf74b739257e21088da87e
sovuix <sovuix@gmail.com>
2025-01-12 13:18:55 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,32 +1,9 @@
-import path from 'node:path';
-import fs from 'node:fs';
 import _ from 'lodash';
-import parse from './parser.js';
 import buildAst from './buildAst.js';
-import plain from './formatters/plain.js';
-import stylish from './formatters/stylish.js';
-import { json } from 'node:stream/consumers';
+import format from './formatters/index.js';
+import getFileData from './getFileData.js';
 
-const getAbsolutePath = (filepath) => path.resolve(process.cwd(), filepath);
-const getFileFormat = (filepath) => path.extname(filepath).slice(1);
 
-const getFileData = (filepath) => {
-  const absoluteFilePath = getAbsolutePath(filepath);
-  const format = getFileFormat(filepath);
-  const data = parse(fs.readFileSync(absoluteFilePath, 'utf8'), format);
-  return data;
-};
-
-const format = (ast, formatName) => {
-  switch(formatName) {
-    case 'json':
-      return JSON.stringify(ast, null, 2);
-    case 'stylish':
-      return stylish(ast);
-    case 'plain': 
-      return plain(ast); 
-  }
-}
 
 const gendiff = (filepath1, filepath2, formatName = 'stylish') => {
   const data1 = getFileData(filepath1);
@@ -36,5 +13,5 @@ const gendiff = (filepath1, filepath2, formatName = 'stylish') => {
 }
 
 export default gendiff;
-// export default getFileData;
+
 
