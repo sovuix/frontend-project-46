f8cacf3836f73ff2baa7919c82a4fda2bbcdb26c
sovuix_gob <sovuix@gmail.com>
2025-01-17 09:30:26 +0300

    fixed

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,33 +1,26 @@
 import fs from 'node:fs';
-import gendiff from '../src/index.js';
 import path from 'node:path';
 import process from 'node:process';
-import { describe } from 'node:test';
-import test from 'node:test';
-import expect from 'node:test';
+import gendiff from '../src/index.js';
 
 const expectedFileStylish = fs.readFileSync(
   './__fixtures__/expectedStylish_JSON.txt',
-  'utf-8'
+  'utf-8',
 );
 
 const expectedFilePlain = fs.readFileSync(
   './__fixtures__/expectedPlain_JSON.txt',
-  'utf-8'
+  'utf-8',
 );
 
-// const expectedFileJson = fs
-//   .readFileSync('./__fixtures__/filejson.json', 'utf-8');
-
-const resultPath = (filename) =>
-  path.join(process.cwd(), '__fixtures__', filename);
+const resultPath = (filename) => path.join(process.cwd(), '__fixtures__', filename);
 
 describe('gendiff', () => {
   test('stylish', () => {
     const result = gendiff(
       resultPath('file1.json'),
       resultPath('file2.json'),
-      'stylish'
+      'stylish',
     );
     expect(result.trim()).toEqual(expectedFileStylish);
   });
@@ -36,13 +29,8 @@ describe('gendiff', () => {
     const result = gendiff(
       resultPath('file1.json'),
       resultPath('file2.json'),
-      'plain'
+      'plain',
     );
     expect(result.trim()).toEqual(expectedFilePlain);
   });
-
-  // test('json', () => {
-  //       const result = JSON.parse(gendiff(resultPath('file1.json'), resultPath('file2.json'), 'json'));
-  //        expect (result).toEqual(JSON.parse(expectedFileJson));
-  //     });
 });
