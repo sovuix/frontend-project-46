6f0437cded920a83715d5e04c7a9f9e31e74dd21
sovuix_gob <sovuix@gmail.com>
2025-01-17 09:29:09 +0300

    fixed

--- src/compare.js
+++ src/compare.js
@@ -3,29 +3,31 @@ import _ from 'lodash';
 const compareKeys = (obj1, obj2) => {
   const keys1 = _.sortBy(Object.keys(obj1));
   const keys2 = _.sortBy(Object.keys(obj2));
-  const result = [];
 
-  for (const key of keys1) {
-    if (!(key in obj2)) {
-      result.push(`- ${key}: ${obj1[key]}`);
-    } else if (obj1[key] !== obj2[key]) {
-      result.push(`- ${key}: ${obj1[key]}`);
-    } else if (obj1[key] === obj2[key]) {
-      result.push(`  ${key}: ${obj1[key]}`);
-    }
-  }
-  for (const key of keys2) {
-    if (!(key in obj1)) {
-      result.push(`+ ${key}: ${obj2[key]}`);
-    } else if (obj1[key] !== obj2[key]) {
-      result.push(`+ ${key}: ${obj2[key]}`);
-    }
-  }
+  const result = [
+    ...keys1
+      .filter((key) => !(key in obj2))
+      .map((key) => `- ${key}: ${obj1[key]}`),
+
+    ...keys1
+      .filter((key) => key in obj2 && obj1[key] !== obj2[key])
+      .map((key) => `- ${key}: ${obj1[key]}`),
+
+    ...keys1
+      .filter((key) => key in obj2 && obj1[key] === obj2[key])
+      .map((key) => `  ${key}: ${obj1[key]}`),
+
+    ...keys2
+      .filter((key) => !(key in obj1))
+      .map((key) => `+ ${key}: ${obj2[key]}`),
+
+    ...keys2
+      .filter((key) => key in obj1 && obj1[key] !== obj2[key])
+      .map((key) => `+ ${key}: ${obj2[key]}`),
+  ];
 
   const formattedResult = result.join('\n  ');
   return `{\n  ${formattedResult}\n}`;
-  
 };
-
 
 export default compareKeys;
